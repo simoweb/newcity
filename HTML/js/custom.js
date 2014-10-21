d1979215fe7383eabb3f88a85d7e8de1a56afbf0
simoweb <simone.puliti@gmail.com>
2014-10-21 13:27:41 +0200

    Modifiche varie

--- HTML/js/custom.js
+++ HTML/js/custom.js
@@ -20,6 +20,65 @@ jQuery("document").ready(function($){
 	});
 });
 
+//slide
+$(document).ready(function() {
+	
+	$("#owl-slide").owlCarousel({
+		animateOut: 'fadeOut',
+		animateIn: 'fadeIn',
+		navigation : true, // Show next and prev buttons
+		nav:true,
+		navText: [
+		"<i class='fa fa-chevron-left'></i>",
+		"<i class='fa fa-chevron-right'></i>"
+		],
+		dots: false,
+		singleItem:true,
+		transitionStyle : "fadeIn",
+		loop:true,
+		items : 1,
+		// "singleItem:true" is a shortcut for:
+		// itemsDesktop : false,
+		// itemsDesktopSmall : false,
+		// itemsTablet: false,
+		// itemsMobile : false
+	});
+});
+
+
+//carousel testimonial footer
+jQuery(document).ready(function() {
+	$('#owl-testimonial').owlCarousel({
+		loop:true,
+		margin:10,
+		nav:true,
+		navText: [
+		"<i class='fa fa-chevron-left'></i>",
+		"<i class='fa fa-chevron-right'></i>"
+		],
+		responsive:{
+			1000:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			200:{
+				items:1
+			}
+		}
+	})
+});
+
+
+//mixtup
+$(function(){
+	$('#grid').mixItUp({
+		animation: {
+			animateResizeTargets: true
+		}
+	});
+})
 //***************************************************
 
 // 2 - switch color demo
@@ -60,6 +119,15 @@ jQuery(document).ready(function() {
 
 //***************************************************
 // 4 - animate on scroll
+
+jQuery(document).ready(function() {
+	jQuery('.fade-scroll').addClass("hidden-scroll").viewportChecker({
+		classToAdd: 'visible-scroll animated fadeIn',
+		offset: 100,
+		callbackFunction: function(elem, action){$('.timer').countTo();} // Callback to do after a class was added to an element. Action will return "add" or "remove", depending if the class was added or removed
+	});
+});
+
 jQuery(document).ready(function() {
     jQuery('.fade-scroll').addClass("hidden-scroll").viewportChecker({
 		classToAdd: 'visible-scroll animated fadeIn',
@@ -72,18 +140,20 @@ jQuery(document).ready(function() {
 	jQuery('.fade-scroll-right').addClass("hidden-scroll").viewportChecker({
 		classToAdd: 'visible-scroll animated fadeInRight',
 		offset: 100
-	});   
-});
-
-//***************************************************
-$(window).stellar({
-	horizontalScrolling: false,
-});
-// 5 - parallax disable on mobile
-
-jQuery(document).ready(function ($) {
-	if (Modernizr.touch){
-		$('.parallax').css('background-attachment', 'scroll');
-	} 
-});
-//***************************************************																						
+		});   
+	});
+	
+	//***************************************************
+	jQuery.stellar({ 
+		horizontalScrolling: false, 
+		hideDistantElements: false, 
+		responsive: true 
+	});
+	// 5 - parallax disable on mobile
+	
+	jQuery(document).ready(function ($) {
+		if (Modernizr.touch){
+			$('.parallax').css('background-attachment', 'scroll');
+		} 
+	});
+	//***************************************************																							
